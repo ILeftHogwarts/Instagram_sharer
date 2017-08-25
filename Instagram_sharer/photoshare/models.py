babab65b439e22d0e86976bfa4cc16a7d82c0be0
ILeftHogwarts <liken2134@gmail.com>
2017-08-25 11:32:00 +0300

    initial migration

--- Instagram_sharer/photoshare/models.py
+++ Instagram_sharer/photoshare/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class InstaPost(models.Model):
-    user_name = models.CharField(max_lenght = 50)
+    user_name = models.CharField(max_length = 50)
     media_file = models.FileField()
-    tag = models.CharField(max_lenght = 50)
+    tag = models.CharField(max_length = 50)
     description = models.TextField()
-    
+
 class User(models.Model):
-    name = models.CharField(max_lenght = 50)
-    sername = models.CharField(max_lenght = 50)
-    access_token = models.CharField(max_lenght = 100)
-    user_post = models.ForeignKey(InstaPost, related_name=InstaPost.user_name, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 50)
+    sername = models.CharField(max_length = 50)
+    access_token = models.CharField(max_length = 100)
+    user_post = models.ForeignKey(InstaPost, on_delete=models.CASCADE)
