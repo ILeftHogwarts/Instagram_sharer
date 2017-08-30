5a1ab9833ed0860b14c5bca96f243455f09eab0a
ILeftHogwarts <liken2134@gmail.com>
2017-08-30 21:18:44 +0300

    requered select

--- Instagram_sharer/photoshare/forms.py
+++ Instagram_sharer/photoshare/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from .models import PostTags
 
 class TagForm(forms.Form):
-    select_tag = forms.ChoiceField(choices=[])
+    select_tag = forms.ChoiceField(choices=[], required=False)
 
     def __init__ (self, *arg, **kwargs):
         super(TagForm, self).__init__(*arg,**kwargs)
         self.fields['select_tag'].choices = [(" ","All tags")]+[(cur_tag.tag,cur_tag.tag) for cur_tag in PostTags.objects.all()]
+        self.fields['select_tag'].initial = [(" ","All tags")]
